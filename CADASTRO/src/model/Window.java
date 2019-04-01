9f2d0ac4dbec627695f148336855ac49be418bcf
henriquegallodias <henrique0521@hotmail.com>
2019-04-01 19:26:17 -0300

    terminada interface de cadastro em java

---
+++ CADASTRO/src/model/Window.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Window {
+		private String name;
+		private String end;
+		
+		public String getname() {
+			return name;
+		}
+		
+		public void setname(String name) {
+			this.name = name;
+		}
+		
+		public String getend() {
+			return end;
+		}
+		
+		public void setend(String end) {
+			this.end = end;
+		}
+
+	}
+
--- CADASTRO/src/view/Window.java
+++ CADASTRO/src/view/Window.java
@@ -2,6 +2,8 @@ package view;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -9,6 +11,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+import javax.swing.JFormattedTextField;
+import javax.swing.text.MaskFormatter;
+import javax.swing.ButtonGroup;
+
+
 
 public class Window extends JFrame {
 	
@@ -28,85 +35,161 @@ public class Window extends JFrame {
 	JTextField txtendereco = new JTextField();
 	JTextField txtcidade = new JTextField();
 	JTextField txtbairro = new JTextField();
-	JTextField txttelefone = new JTextField();
-	JTextField txtrg = new JTextField();
-	JTextField txtcep = new JTextField();
-	JTextField txtcelular = new JTextField("()   -    ");
-	JTextField txtcpf = new JTextField();
+	
+	JFormattedTextField txttelefone = null;
+	JFormattedTextField txtrg = null;
+	JFormattedTextField txtcep = null;
+	JFormattedTextField txtcelular = null;
+	JFormattedTextField txtcpf = null;
 	
 	JButton jbsalvar = new JButton("Salvar");
 	JButton jbimprimir = new JButton("Imprimir");
 	
 	JComboBox jcbest = new JComboBox(new Object[] {"AC","SP","RJ","GO","PR","BA","MA","PA","SC","RR","RS","TO","ES","MG","AL","PE","PB","MT","MS","CE"});
 	
-	JRadioButton jrbsex = new JRadioButton("Feminino");
-	JRadioButton jrbsexm = new JRadioButton("Masculino");
+	JRadioButton[] jrbsexo = new JRadioButton[2]; 
+	ButtonGroup Control = new ButtonGroup();{ 
 	
+	jrbsexo[0] = new JRadioButton("Feminino");
+	jrbsexo[1] = new JRadioButton("Masculino");} 
+	
+	MaskFormatter formatTel = null;
+	MaskFormatter formatCel = null;
+	MaskFormatter formatCpf = null;
+	MaskFormatter formatCep = null;
+	MaskFormatter formatRg = null;
+	
+	model.Window aa = new model.Window();
 	
 	public Window() {
 		super("CADASTRO");
 		
 		Container paine = this.getContentPane();
 	
-		
 		paine.add(lblnome);
 		lblnome.setBounds(15, 25, 60, 10);
+		
 		paine.add(lblendereco);
 		lblendereco.setBounds(15, 65, 60, 10);
+		
 		paine.add(lblcidade);
 		lblcidade.setBounds(15, 105, 60, 10);
+		
 		paine.add(lblbairro);
 		lblbairro.setBounds(15, 145, 60, 10);
+		
 		paine.add(lbltelefone);
 		lbltelefone.setBounds(15, 185, 60, 10);
+		
 		paine.add(lblrg);
 		lblrg.setBounds(15,225,60,10);
+		
 		paine.add(lblsexo);
 		lblsexo.setBounds(320, 25, 60, 10);
+		
 		paine.add(lblcep);
 		lblcep.setBounds(320, 65, 60, 10);
+		
 		paine.add(lblestado);
 		lblestado.setBounds(320, 105, 60, 10);
+		
 		paine.add(lblcelular);
 		lblcelular.setBounds(320,145,60,10);
+		
 		paine.add(lblcpf);
 		lblcpf.setBounds(320, 185, 60, 10);
+		
 		paine.add(jcbest);
 		jcbest.setBounds(395, 95, 180, 30);
-		paine.add(jrbsex);
-		jrbsex.setBounds(390, 25, 90, 12);
-		paine.add(jrbsexm);
-		jrbsexm.setBounds(480, 25, 90, 12);
+		jcbest.setBackground(Color.orange);
+		
+	    Control.add(jrbsexo[0]); 
+		Control.add(jrbsexo[1]);
+		
+		paine.add(jrbsexo[0]); 
+		jrbsexo[0].setBounds(390, 25, 90, 12);
+		paine.add(jrbsexo[1]);
+		jrbsexo[1].setBounds(480, 25, 90, 12);
+		jrbsexo[0].setBackground(Color.orange);
+		jrbsexo[1].setBackground(Color.orange);
+		
 		paine.add(jbsalvar);
 		jbsalvar.setBounds(160, 270, 120, 30);
+		jbsalvar.setBackground(Color.orange);
+		jbsalvar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				aa.setname(txtnome.getText());
+				aa.setend(txtendereco.getText());
+			}
+		});
+		
 		paine.add(jbimprimir);
 		jbimprimir.setBounds(285, 270, 120, 30);
+		jbimprimir.setBackground(Color.orange);
+		
 		paine.add(txtnome);
 		txtnome.setBounds(90, 15, 180, 30);
+		
 		paine.add(txtendereco);
 		txtendereco.setBounds(90, 55, 180, 30);
+		
 		paine.add(txtcidade);
 		txtcidade.setBounds(90, 95, 180, 30);
+		
 		paine.add(txtbairro);
 		txtbairro.setBounds(90, 135, 180, 30);
+		
+		try {
+			formatTel = new MaskFormatter("(##)####-####");
+			txttelefone = new JFormattedTextField(formatTel);
+		} catch (Exception ext) {
+			ext.printStackTrace();
+		}
 		paine.add(txttelefone);
 		txttelefone.setBounds(90, 175, 180, 30);
+		
+		try {
+			formatRg = new MaskFormatter("##.###.###-A");
+			txtrg = new JFormattedTextField(formatRg);
+		} catch (Exception exr) { 
+			exr.printStackTrace();
+		}
 		paine.add(txtrg);
 		txtrg.setBounds(90, 215, 180, 30);
+		
+		try { 
+			formatCep = new MaskFormatter("#####-###");
+			txtcep = new JFormattedTextField(formatCep);
+		} catch (Exception ex) { 
+			ex.printStackTrace();
+		}
 		paine.add(txtcep);
 		txtcep.setBounds(395, 55, 180, 30);
+		
+		try {
+			formatCel = new MaskFormatter("(##)#####-####"); 
+			txtcelular = new JFormattedTextField(formatCel);
+		} catch (Exception exc) { 
+			exc.printStackTrace();
+		}
 		paine.add(txtcelular);
 		txtcelular.setBounds(395, 135, 180, 30);
+		
+		try {
+			formatCpf = new MaskFormatter("###.###.###-##"); 
+			txtcpf = new JFormattedTextField(formatCpf);
+		} catch (Exception exf) { 
+			exf.printStackTrace();
+		}
 		paine.add(txtcpf);
 		txtcpf.setBounds(395, 175, 180, 30);
 		
 		this.setLayout(null);
-		
 		this.setSize(600,350);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.setVisible(true);
-		this.getContentPane().setBackground(Color.ORANGE);
+		this.getContentPane().setBackground(Color.orange);
 	}
 	public static void main(String[] args) {
 		Window w = new Window();
